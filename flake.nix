b35ab87d49a90cd476faebb79f78b060f4c73416
Sam Mahoney <mahoney@cmui.co.uk>
2025-12-02 15:37:57 +0000

    home-manager!

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      # ensure nixpkgs is kept consistent throughout the flake
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ...}@inputs:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, ...}@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -17,6 +22,14 @@
         modules = [
 	  nixos-hardware.nixosModules.dell-precision-5570
 	  ./configuration.nix
+	  # auto deploy home-manager with `nixos-rebuild switch`
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mahoney = import ./home.nix;
+	    # home-manager.extraSpecialArgs to pass args to home.nix
+          }
         ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ...}:
+
+{
+  home.username = "mahoney";
+  home.homeDirectory = "/home/mahoney";
+
+  home.packages = with pkgs; [
+    # utils
+    ripgrep
+    jq
+    zip
+    xz
+    unzip
+    fzf
+    
+    # network tools
+    mtr
+    iperf3
+    dnsutils
+    ldns  # provides `drill` command, `dig` replacement
+    nmap
+    ipcalc
+
+    # misc
+    file
+    which 
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+    
+    # term tools
+    glow  # markdown in the terminal
+    btop
+    iotop
+    iftop
+    nvtopPackages.nvidia
+
+    # system call stuff
+    strace  # system call monitoring
+    ltrace  # library call monitoring
+    lsof  # list open files
+
+    # system tools
+    fastfetch
+    sysstat
+    lm_sensors
+    ethtool
+    pciutils  # lspci
+    usbutils  # lsusb
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Sam Mahoney";
+    userEmail = "mahoney@cmui.co.uk";  # default to personal
+
+   extraConfig = {
+     core.sshCommand = "ssh -i ~/.ssh/helios_personal_ed25519";
+    };
+  
+  includes = [
+    {
+      # use work SSH key in work dir
+      condition = "gitdir:~/cydar/";
+      contents = {
+        user = {
+	  name = "Sam Mahoney";
+          email = "sam.mahoney@cydar.co.uk";
+        };
+        core.sshCommand = "ssh -i ~/.ssh/helios_ed25519";
+      };
+    }
+    ];
+  };
+  
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.alacritty = {
+    settings = {
+      env.TERM = "xterm-256color";
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  home.stateVersion = "25.11";
+}
