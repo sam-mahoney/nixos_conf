7807d9bd04e24d88d5358e468bfedb47970b4977
Sam Mahoney <mahoney@cmui.co.uk>
2026-03-13 21:43:11 +0000

    theme neovim to match terminal palette

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -54,6 +54,135 @@
       vim.g.mapleader = " "
       vim.g.maplocalleader = ","
 
+      local palette = {
+        bg = "#000000",
+        bg_alt = "#111111",
+        fg = "#d8d8d8",
+        fg_bright = "#e6e6e6",
+        white = "#ffffff",
+        gray_1 = "#c0c0c0",
+        gray_2 = "#a0a0a0",
+        gray_3 = "#9a9a9a",
+        gray_4 = "#6f6f6f",
+        gray_5 = "#3a3a3a",
+        red = "#ff8080",
+      }
+
+      local set_hl = vim.api.nvim_set_hl
+      vim.cmd("highlight clear")
+      vim.cmd("syntax reset")
+      vim.o.background = "dark"
+      vim.g.colors_name = "terminal_mono"
+
+      set_hl(0, "Normal", { fg = palette.fg, bg = palette.bg })
+      set_hl(0, "NormalNC", { fg = palette.fg, bg = palette.bg })
+      set_hl(0, "NormalFloat", { fg = palette.fg, bg = palette.bg_alt })
+      set_hl(0, "FloatBorder", { fg = palette.gray_5, bg = palette.bg_alt })
+      set_hl(0, "FloatTitle", { fg = palette.gray_1, bg = palette.bg_alt, bold = true })
+      set_hl(0, "CursorLine", { bg = palette.bg_alt })
+      set_hl(0, "CursorColumn", { bg = palette.bg_alt })
+      set_hl(0, "ColorColumn", { bg = palette.bg_alt })
+      set_hl(0, "CursorLineNr", { fg = palette.white, bg = palette.bg_alt, bold = true })
+      set_hl(0, "LineNr", { fg = palette.gray_4, bg = palette.bg })
+      set_hl(0, "SignColumn", { fg = palette.gray_4, bg = palette.bg })
+      set_hl(0, "VertSplit", { fg = palette.gray_5, bg = palette.bg })
+      set_hl(0, "WinSeparator", { fg = palette.gray_5, bg = palette.bg })
+      set_hl(0, "StatusLine", { fg = palette.fg_bright, bg = palette.bg_alt })
+      set_hl(0, "StatusLineNC", { fg = palette.gray_4, bg = palette.bg_alt })
+      set_hl(0, "TabLine", { fg = palette.gray_4, bg = palette.bg_alt })
+      set_hl(0, "TabLineFill", { fg = palette.gray_4, bg = palette.bg })
+      set_hl(0, "TabLineSel", { fg = palette.white, bg = palette.bg, bold = true })
+      set_hl(0, "Pmenu", { fg = palette.fg, bg = palette.bg_alt })
+      set_hl(0, "PmenuSel", { fg = palette.bg, bg = palette.gray_3, bold = true })
+      set_hl(0, "PmenuSbar", { bg = palette.gray_5 })
+      set_hl(0, "PmenuThumb", { bg = palette.gray_2 })
+      set_hl(0, "Visual", { fg = palette.bg, bg = palette.gray_3 })
+      set_hl(0, "Search", { fg = palette.bg, bg = palette.gray_3 })
+      set_hl(0, "IncSearch", { fg = palette.bg, bg = palette.gray_1, bold = true })
+      set_hl(0, "CurSearch", { fg = palette.bg, bg = palette.gray_1, bold = true })
+      set_hl(0, "MatchParen", { fg = palette.white, bg = palette.gray_5, bold = true })
+      set_hl(0, "Directory", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Title", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Question", { fg = palette.gray_1, bold = true })
+      set_hl(0, "NonText", { fg = palette.gray_5 })
+      set_hl(0, "Whitespace", { fg = palette.gray_5 })
+      set_hl(0, "EndOfBuffer", { fg = palette.bg })
+
+      set_hl(0, "Comment", { fg = palette.gray_4, italic = true })
+      set_hl(0, "Constant", { fg = palette.gray_1 })
+      set_hl(0, "String", { fg = palette.fg_bright })
+      set_hl(0, "Character", { fg = palette.fg_bright })
+      set_hl(0, "Number", { fg = palette.gray_2 })
+      set_hl(0, "Boolean", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Float", { fg = palette.gray_2 })
+      set_hl(0, "Identifier", { fg = palette.fg })
+      set_hl(0, "Function", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Statement", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Conditional", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Repeat", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Label", { fg = palette.gray_2 })
+      set_hl(0, "Operator", { fg = palette.gray_2 })
+      set_hl(0, "Keyword", { fg = palette.gray_1, bold = true })
+      set_hl(0, "Exception", { fg = palette.red, bold = true })
+      set_hl(0, "PreProc", { fg = palette.gray_2 })
+      set_hl(0, "Include", { fg = palette.gray_2, bold = true })
+      set_hl(0, "Define", { fg = palette.gray_2 })
+      set_hl(0, "Macro", { fg = palette.gray_2 })
+      set_hl(0, "PreCondit", { fg = palette.gray_2 })
+      set_hl(0, "Type", { fg = palette.gray_1 })
+      set_hl(0, "StorageClass", { fg = palette.gray_2 })
+      set_hl(0, "Structure", { fg = palette.gray_1 })
+      set_hl(0, "Typedef", { fg = palette.gray_1 })
+      set_hl(0, "Special", { fg = palette.gray_2 })
+      set_hl(0, "SpecialComment", { fg = palette.gray_3, italic = true })
+      set_hl(0, "Underlined", { fg = palette.gray_1, underline = true })
+      set_hl(0, "Todo", { fg = palette.bg, bg = palette.gray_1, bold = true })
+      set_hl(0, "Error", { fg = palette.red, bold = true })
+      set_hl(0, "ErrorMsg", { fg = palette.red, bold = true })
+      set_hl(0, "WarningMsg", { fg = palette.gray_1, bold = true })
+
+      set_hl(0, "DiagnosticError", { fg = palette.red })
+      set_hl(0, "DiagnosticWarn", { fg = palette.gray_1 })
+      set_hl(0, "DiagnosticInfo", { fg = palette.gray_2 })
+      set_hl(0, "DiagnosticHint", { fg = palette.gray_3 })
+      set_hl(0, "DiagnosticOk", { fg = palette.fg_bright })
+      set_hl(0, "DiagnosticVirtualTextError", { fg = palette.red, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticVirtualTextWarn", { fg = palette.gray_1, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticVirtualTextInfo", { fg = palette.gray_2, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticVirtualTextHint", { fg = palette.gray_3, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = palette.red })
+      set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = palette.gray_1 })
+      set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, sp = palette.gray_2 })
+      set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = palette.gray_3 })
+      set_hl(0, "DiagnosticFloatingError", { fg = palette.red, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticFloatingWarn", { fg = palette.gray_1, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticFloatingInfo", { fg = palette.gray_2, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticFloatingHint", { fg = palette.gray_3, bg = palette.bg_alt })
+      set_hl(0, "DiagnosticSignError", { fg = palette.red, bg = palette.bg })
+      set_hl(0, "DiagnosticSignWarn", { fg = palette.gray_1, bg = palette.bg })
+      set_hl(0, "DiagnosticSignInfo", { fg = palette.gray_2, bg = palette.bg })
+      set_hl(0, "DiagnosticSignHint", { fg = palette.gray_3, bg = palette.bg })
+
+      set_hl(0, "GitSignsAdd", { fg = palette.fg_bright, bg = palette.bg })
+      set_hl(0, "GitSignsChange", { fg = palette.gray_1, bg = palette.bg })
+      set_hl(0, "GitSignsDelete", { fg = palette.red, bg = palette.bg })
+
+      set_hl(0, "TelescopeNormal", { fg = palette.fg, bg = palette.bg_alt })
+      set_hl(0, "TelescopeBorder", { fg = palette.gray_5, bg = palette.bg_alt })
+      set_hl(0, "TelescopeTitle", { fg = palette.gray_1, bg = palette.bg_alt, bold = true })
+      set_hl(0, "TelescopePromptNormal", { fg = palette.fg_bright, bg = palette.bg_alt })
+      set_hl(0, "TelescopePromptBorder", { fg = palette.gray_5, bg = palette.bg_alt })
+      set_hl(0, "TelescopePromptPrefix", { fg = palette.gray_2, bg = palette.bg_alt })
+      set_hl(0, "TelescopeSelection", { fg = palette.bg, bg = palette.gray_3, bold = true })
+      set_hl(0, "TelescopeSelectionCaret", { fg = palette.bg, bg = palette.gray_3, bold = true })
+      set_hl(0, "TelescopeMatching", { fg = palette.white, bold = true })
+
+      set_hl(0, "CmpItemAbbr", { fg = palette.fg })
+      set_hl(0, "CmpItemAbbrMatch", { fg = palette.white, bold = true })
+      set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = palette.gray_1, bold = true })
+      set_hl(0, "CmpItemMenu", { fg = palette.gray_4 })
+      set_hl(0, "CmpItemKind", { fg = palette.gray_2 })
+
       local opt = vim.opt
       opt.number = true
       opt.relativenumber = true
